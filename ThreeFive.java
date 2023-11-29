df3d2338c4b37207d3852618956480f6f945b87a
BoljevicD <bojevic.dus4n@gmail.com>
2023-11-29 10:30:52 +0200

    ...

--- ThreeFive.java
+++ ThreeFive.java
@@ -2,16 +2,16 @@ public class ThreeFive {
 
     public static void main(String[] args) {
         int limit = 1000;
-        int sum = calculateMultiplesSum(limit);
+        int sum = MultiplesSum(limit);
 
         System.out.println("The sum of multiples of 3 or 5 below 1000 is: " + sum);
     }
 
-    private static int calculateMultiplesSum(int limit) {
+    private static int MultiplesSum(int limit) {
         int sum = 0;
 
-        for (int i = 1; i < limit; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
+        for(int i = 1; i < limit; i++){
+            if (i % 3 == 0 || i % 5 == 0){
                 sum += i;
             }
         }
